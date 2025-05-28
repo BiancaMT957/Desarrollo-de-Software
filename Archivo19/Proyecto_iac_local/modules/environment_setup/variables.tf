d8fccec3ed599a4fc10b748e46d9ba3746b4bdbd
BiancaMT <merchantorresbianca75@gmail.com>
2025-05-28 02:34:35 -0400

    Add files via upload

---
+++ Archivo19/Proyecto_iac_local/modules/environment_setup/variables.tf
@@ -0,0 +1,11 @@
+variable "base_path" {
+  description = "Ruta base para el entorno."
+  type        = string
+}
+
+variable "nombre_entorno_modulo" {
+  description = "Nombre del entorno para este módulo."
+  type        = string
+}
+
+
